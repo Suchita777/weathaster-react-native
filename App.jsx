e5ca976fb5fc403ee8c7e64078002dc56f679e3b
Suchita777 <suchitamishra318@gmail.com>
2023-04-17 17:56:13 +0530

    FEAT : Working Notifications for news

--- App.jsx
+++ App.jsx
@@ -1,10 +1,60 @@
-import { View, Platform, StatusBar } from "react-native";
+import { useState, useEffect, useRef } from "react";
+import { View, Platform, StatusBar, Text } from "react-native";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 import { WebView } from "react-native-webview";
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      await schedulePushNotification();
+    }, 10000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View className="flex-1 flex-col">
       <StatusBar />
+      <Text className="hidden">
+        {JSON.stringify(expoPushToken)}
+        {JSON.stringify(notification)}
+      </Text>
       <WebView
         originWhitelist={["*"]}
         source={{
@@ -13,4 +63,50 @@ export default function App() {
       />
     </View>
   );
+}
+
+// Logic for sending push notifications and regestering the notification handler
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "🚨 You've got a news update!!",
+      body: "Click here to view the latest disaster news",
+      data: { data: "goes here" },
+    },
+    trigger: { seconds: 1 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  return token;
 }
